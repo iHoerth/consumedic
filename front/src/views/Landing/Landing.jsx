f8dcdd373875ce5e17a2d6463858c7c280bad9a2
iHoerth <hoerth.ignacio@gmail.com>
2023-05-08 15:09:27 -0300

    Probando NavBar Mui

--- front/src/views/Landing/Landing.jsx
+++ front/src/views/Landing/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import NavBarMui from '../../components/NavBar/NavBarMUI';
 
 const Landing = () => {
   return (
     <div>
+      <NavBarMui />
       <NavBar />
     </div>
   );
