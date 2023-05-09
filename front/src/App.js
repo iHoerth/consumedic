9fc1f358100e8a868b67a7710460e0bddd204905
iHoerth <hoerth.ignacio@gmail.com>
2023-05-09 11:22:16 -0300

    Creacion theme.js y ThemeProvider

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SpecialistsFound, Landing, Home } from './views';
-import UserLogin from './views/UserLogin/UserLogin'
-import { ThemeProvider } from '@mui/material';
+import { Routes, Route } from 'react-router-dom';
+
+import { Home, Landing, SpecialistsFound, UserLogin } from './views';
+
 
 import './App.css';
 import '@fontsource/roboto/300.css';
@@ -10,14 +10,17 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 function App() {
+
   return (
     <div className="App">
+      {/* <ThemeProvider> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="/login" element={<UserLogin />} />
         <Route path="/all" element={<SpecialistsFound />} />
       </Routes>
+      {/* </ThemeProvider> */}
     </div>
   );
 }
--- front/src/views/index.js
+++ front/src/views/index.js
@@ -1,8 +1,8 @@
 import Home from './Home/Home';
 import Landing from './Landing/Landing';
 import SpecialistsFound from './SpecialistsFound/SpecialistsFound';
+import UserLogin from './UserLogin/UserLogin'
 
-
-export { Home, Landing, SpecialistsFound };
+export { Home, Landing, SpecialistsFound, UserLogin };
 
 
