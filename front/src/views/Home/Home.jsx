fd05c59325b49612348fc56300fcf70af48e0b52
dalegaz <indicevertical@gmail.com>
2023-05-09 22:14:39 -0300

    DarkMode

--- front/src/views/Home/Home.jsx
+++ front/src/views/Home/Home.jsx
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
-import React from "react";
-
-const Home = () => {
-    return ( <div>HOME</div>)
-}
-
-export default Home;
-=======
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import SearchBar from "../../components/SearchBar/SearchBar"
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <SearchBar />
     </div>
   )
 }
 
 export default Home
->>>>>>> c43855b5f8b04016e699f584f7d0e6bea699c158
