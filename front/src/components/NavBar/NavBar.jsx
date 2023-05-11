3666e77d06974ea0aa293d6c21551b43e9f2b201
noelia-paz <noeliapaz676@gmail.com>
2023-05-11 15:19:09 -0300

    Create Component ImageHome.jsx

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -1,41 +1,41 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { useMediaQuery, useTheme } from '@mui/material';
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useMediaQuery, useTheme } from "@mui/material";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
 
-import DrawerComponent from './DrawerComponent';
+import DrawerComponent from "./DrawerComponent";
 
 const NavBar = () => {
   const theme = useTheme();
-  const screenSizeSmall = useMediaQuery(theme.breakpoints.down('tablet'));
+  const screenSizeSmall = useMediaQuery(theme.breakpoints.down("tablet"));
   const { values } = theme.breakpoints;
 
-  console.log(theme.breakpoints.down('tablet'));
+  console.log(theme.breakpoints.down("tablet"));
   const navLinksArray = [
     {
-      title: 'Home',
-      path: '/',
+      title: "Home",
+      path: "/",
     },
     {
-      title: 'Login',
-      path: '/login',
+      title: "Login",
+      path: "/login",
     },
     {
-      title: 'Eres un medico?',
-      path: '/login',
+      title: "Eres un medico?",
+      path: "/login",
     },
   ];
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1, width: "100%" }}>
       <AppBar
         color="secondary"
         position="static"
-        sx={{ height: '100px', justifyContent: 'center', alignItems: 'center' }}
+        sx={{ height: "100px", justifyContent: "center", alignItems: "center" }}
       >
         <Toolbar
           color="white"
@@ -43,14 +43,20 @@ const NavBar = () => {
           alignContent="center"
           sx={{
             width: {
-              mobile: '100vw',
-              tablet: '100vw',
-              laptop: '100vw',
+              mobile: "100vw",
+              tablet: "100vw",
+              laptop: "100vw",
               desktop: values.desktop,
             },
           }}
         >
-          <Box display="flex" alignItems="center" gap="4px" color="white" sx={{ flexGrow: 1 }}>
+          <Box
+            display="flex"
+            alignItems="center"
+            gap="4px"
+            color="white"
+            sx={{ flexGrow: 1 }}
+          >
             <LocalHospitalIcon color="inherit" />
             <Typography variant="h5" component="div" color="white">
               CONSUMEDIC
@@ -59,7 +65,7 @@ const NavBar = () => {
           {screenSizeSmall ? (
             <DrawerComponent navLinksArray={navLinksArray} />
           ) : (
-            <nav style={{ color: 'white' }}>
+            <nav style={{ color: "white" }}>
               {navLinksArray.map((link) => (
                 <Button color="inherit" href={link.path}>
                   {link.title}
