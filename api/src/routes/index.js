35ef0f59bcc79581e1e5fd1f4434ce44b2779e9b
Maurog5 <maurogatica015@gmail.com>
2023-05-11 16:53:54 -0300

    cree las rutas de citas post y get

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -6,7 +6,8 @@ const specialties = require("./specialties");
 const opinions = require("./opinions");
 const clinicHistory = require("./clinicHistory");
 const payments = require("./payments");
-
+const createCita = require("./createCita");
+const citaRoutes = require('./citaRoutes');
 //!fake data
 const { createFakeData } = require("../fakeData/fakeData");
 
@@ -28,7 +29,8 @@ router.use("/specialties", specialties);
 router.use("/opinions", opinions);
 router.use("/clinicHistory", clinicHistory);
 router.use("/payments", payments);
-
+router.use("/citas", createCita); 
+router.use('/citas', citaRoutes);
 
 
 module.exports = router;
