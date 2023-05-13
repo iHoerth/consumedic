a608f522eb13afc2b831bef7fae95798effb0110
facuaragon <facundo_aragon@hotmail.com>
2023-05-12 21:14:29 -0300

    ruta para traer claendario creada y faker modificada

--- api/src/routes/appointments.js
+++ api/src/routes/appointments.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 
-const { createAppointments, getAppointments, getAppointmentsDoctor, getAppointmentsPatient } = require('../handlers/appointments');
+const { createAppointments, getAppointments, getAppointmentsDoctor, getAppointmentsPatient, getCalendar } = require('../handlers/appointments');
 
 const appointmentsRouter = Router();
 
@@ -11,6 +11,6 @@ appointmentsRouter.post('/', createAppointments);
 appointmentsRouter.get('/', getAppointments);
 appointmentsRouter.get('/doctor/:id', getAppointmentsDoctor);
 appointmentsRouter.get('/patient/:id', getAppointmentsPatient);
-
+appointmentsRouter.get('/calendar/:id', getCalendar);
 
 module.exports = appointmentsRouter;
