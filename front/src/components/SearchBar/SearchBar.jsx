ded3198efbd8402d2d0c2534d2f426c629c8f022
Maurog5 <maurogatica015@gmail.com>
2023-05-13 17:56:00 -0300

    searchbar estilizada

--- front/src/components/SearchBar/SearchBar.jsx
+++ front/src/components/SearchBar/SearchBar.jsx
@@ -10,36 +10,36 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { NavLink } from 'react-router-dom';
-import { useMediaQuery, useTheme } from '@mui/material';
 
 const ProminentAppBar = () => {
-  const theme = useTheme();
-
   return (
-    <Box sx={{  width: '70%' }}>
-      <AppBar position="static">
-        <Toolbar color="secondary">
-          <FormControl sx={{ minWidth: 150, color: 'white' }}>
-            <InputLabel id="especialista-label">Especialista</InputLabel>
+    <Box sx={{ flexGrow: 1, width: '65%', borderRadius: '8px' }}>
+      <AppBar position="static" style={{ background: '#00B5F9', borderRadius: '10px' }}>
+        <Toolbar>
+          <FormControl variant="outlined" sx={{ minWidth: 200, marginRight: '3rem' }}>
+            <InputLabel id="especialista-label" style={{ color: 'width' }}>
+              Especialista
+            </InputLabel>
             <Select
-              
               labelId="especialista-label"
               id="especialista-select"
               label="Especialista"
               defaultValue=""
+              style={{ color: 'white' }}
             >
               <MenuItem value={10}>Gastroenterólogo</MenuItem>
               <MenuItem value={20}>Médico Clínico</MenuItem>
               <MenuItem value={30}>Odontólogo</MenuItem>
             </Select>
           </FormControl>
-          <FormControl sx={{ minWidth: 120 }}>
-            <InputLabel id="ubicacion-label">Ubicación</InputLabel>
+          <FormControl variant="outlined" sx={{ minWidth: 200 }}>
+            <InputLabel id="ubicacion-label" style={{ color: 'width' }}>Ubicación</InputLabel>
             <Select
               labelId="ubicacion-label"
               id="ubicacion-select"
               label="Ubicación"
               defaultValue=""
+              style={{ color: 'white' }}
             >
               <MenuItem value={1}>Santa Fe</MenuItem>
               <MenuItem value={2}>Buenos Aires</MenuItem>
@@ -47,7 +47,7 @@ const ProminentAppBar = () => {
             </Select>
           </FormControl>
           <NavLink to="/search">
-            <IconButton size="large" aria-label="search" color="inherit">
+            <IconButton size="large" aria-label="search" color="blue" sx={{ marginLeft: '1rem', backgroundColor: '#00D3A4', borderRadius: '50%' }}>
               <SearchIcon />
             </IconButton>
           </NavLink>
