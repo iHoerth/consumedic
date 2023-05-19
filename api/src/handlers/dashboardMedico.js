3439a121b2d45a7bf8d3e0f9e79b63d5ff0513cf
facuaragon <facundo_aragon@hotmail.com>
2023-05-19 19:40:38 -0300

    avances dashboard Medico historial Paciente, rutas de post de comentarios medicos y documentos con cloudinaru

--- api/src/handlers/dashboardMedico.js
+++ api/src/handlers/dashboardMedico.js
@@ -1,6 +1,9 @@
+const cloudinary = require("../utils/cloudinary")
 
 const {getAllPatientsByDoctor} = require("../controllers/patients/getAllPatientsByDoctor")
 const {getPatientHistory} = require("../controllers/patients/getPatientHistory")
+const {postAppointmentDocument}=require("../controllers/appointments/postAppointmentDocument")
+const {postAppointmentResponse}=require("../controllers/appointments/postAppointmentResponse")
 
 const getPatientsByDoctor = async (req, res)=>{
     try {
@@ -23,9 +26,40 @@ const getHistorialPaciente = async (req, res)=>{
     }
 }
 
+const postDoctorResponse = async (req,res)=>{
+    try {
+        const {idCita,respuesta} = req.body
+        console.log(idCita,respuesta);
+        const result = await postAppointmentResponse(idCita,respuesta)
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
+const postDoctorDocument = async (req,res)=>{
+    try {
+        const {idCita,files64, idMedico, idPaciente} = req.body
+
+        console.log(idCita,files64, idMedico, idPaciente);
+
+        const cloudinaryResult = await cloudinary.uploader.upload(files64, {
+            folder: "Documents",
+          })
+          const imagenCloudinary = cloudinaryResult.secure_url;
+
+        const result = await postAppointmentDocument(idCita,imagenCloudinary,idMedico, idPaciente)
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+}
+
 
 
 module.exports = {
     getPatientsByDoctor,
-    getHistorialPaciente
+    getHistorialPaciente,
+    postDoctorResponse,
+    postDoctorDocument
 }
--- api/src/routes/dashboardMedico.js
+++ api/src/routes/dashboardMedico.js
@@ -2,7 +2,10 @@ const { Router } = require("express");
 
 const {
     getPatientsByDoctor,
-    getHistorialPaciente
+    getHistorialPaciente,
+    postDoctorResponse,
+    postDoctorDocument
+
 } = require("../handlers/dashboardMedico");
 
 const dashboardMedicoRouter = Router();
@@ -10,6 +13,11 @@ const dashboardMedicoRouter = Router();
 // GET
 dashboardMedicoRouter.get("/:id/pacientes", getPatientsByDoctor);
 dashboardMedicoRouter.get("/:idMedico/pacientes/:idPaciente", getHistorialPaciente);
+
+//POST
+dashboardMedicoRouter.post("/doctor/cita/respuesta", postDoctorResponse);
+dashboardMedicoRouter.post("/doctor/cita/documento", postDoctorDocument);
+
 
 
 module.exports = dashboardMedicoRouter;
