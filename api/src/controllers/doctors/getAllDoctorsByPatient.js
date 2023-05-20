0ab43e3c3fb4e31122725780fb0bf177ea7fa505
facuaragon <facundo_aragon@hotmail.com>
2023-05-20 10:12:57 -0300

    agregado ruta para panel paciente con info para Noe

---
+++ api/src/controllers/doctors/getAllDoctorsByPatient.js
@@ -0,0 +1,43 @@
+const { Cita, DoctorType, ObraSocial, Especialidad, Opinion } = require("../../db");
+
+const getAllDoctorsByPatient = async (id) => {
+  const citas = await Cita.findAll({
+    where: { PacienteTypeId: id },
+  });
+
+  let doctors = new Set();
+
+  citas.forEach(element => {
+    doctors.add(element.DoctorTypeId)
+  });
+  doctors = [...doctors];
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  doctors.sort(compareNumbers);
+  console.log(doctors);
+  let doctorDetails = [];
+    for(let i=0; i < doctors.length;i++){
+        const doctor = await DoctorType.findOne({ 
+            where: { 
+                id: doctors[i],
+                status: "active"
+            },
+            attributes: {exclude: ['password', "status", "isDoctor"]},
+            include: [
+              {model: Especialidad},
+              {model: ObraSocial},
+              {model: Cita,
+                where:{
+                      PacienteTypeId: id
+                    }
+              },
+            ]
+         });
+
+        doctorDetails.push(doctor)
+    }
+    return doctorDetails;
+};
+
+module.exports = { getAllDoctorsByPatient };
