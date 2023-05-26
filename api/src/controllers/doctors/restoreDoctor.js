e18d0dc2b46b3c7a783491acde45654a8a0b021d
roloegus <70853266+roloegus@users.noreply.github.com>
2023-05-26 10:58:30 +0100

    manejo de errores en controllers restore y delete

--- api/src/controllers/doctors/restoreDoctor.js
+++ api/src/controllers/doctors/restoreDoctor.js
@@ -2,6 +2,13 @@ const { DoctorType } = require("../../db");
 
 const restoreDoctor = async ({ id }) => {
   try {
+    const doctor = await DoctorType.findOne({
+      where: { id: id },
+      paranoid: false,
+    });
+    if (!doctor) {
+      throw new Error(`No existe el doctor con ID ${id}`);
+    }
     await DoctorType.restore({
       where: {
         id: id,
