28c420ef5851041d0dfb96b01080ebc2556a4973
roloegus <70853266+roloegus@users.noreply.github.com>
2023-05-30 17:40:45 +0100

    boton admin muestra solo si es admin en mi cuenta

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -1,42 +1,43 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import { Link, useMediaQuery, useTheme } from '@mui/material';
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
-import { useState, useEffect } from 'react';
-import { useContext } from 'react';
-import DrawerComponent from './DrawerComponent';
-import { Context } from '../../context/ContextProvider';
-import { useNavigate, useLocation } from 'react-router-dom';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link, useMediaQuery, useTheme } from "@mui/material";
+import LocalHospitalIcon from "@mui/icons-material/LocalHospital";
+import { useState, useEffect } from "react";
+import { useContext } from "react";
+import DrawerComponent from "./DrawerComponent";
+import { Context } from "../../context/ContextProvider";
+import { useNavigate, useLocation } from "react-router-dom";
 
-
-const NavBar = ({ component, variant,text, type }) => {
+const NavBar = ({ component, variant, text, type }) => {
   const { session, setSession } = useContext(Context)[2];
+  const { patientDetail, modifyPatientProfiler, fetchPatientByEmail } =
+    useContext(Context)[1];
   const navigate = useNavigate();
   const location = useLocation();
   const theme = useTheme();
   const { values } = theme.breakpoints;
-  const screenSizeSmall = useMediaQuery(theme.breakpoints.down('tablet'));
+  const screenSizeSmall = useMediaQuery(theme.breakpoints.down("tablet"));
 
   const navLinksArray = [
     {
-      title: 'Inicio',
-      path: '/',
+      title: "Inicio",
+      path: "/",
     },
     {
-      title: 'Ingresar',
-      path: '/login',
+      title: "Ingresar",
+      path: "/login",
     },
     {
-      title: 'Eres médico/a?',
-      path: '/loginDoctor',
+      title: "Eres médico/a?",
+      path: "/loginDoctor",
     },
     {
-      title: 'Eres Admin?',
-      path: '/login',
+      title: "Eres Admin?",
+      path: "/login",
     },
   ];
 
@@ -45,7 +46,7 @@ const NavBar = ({ component, variant,text, type }) => {
 
   const handleSessionClose = () => {
     setSession({});
-    navigate('/');
+    navigate("/");
   };
   /*listener de eventos de scroll a la ventana */
   useEffect(() => {
@@ -57,28 +58,27 @@ const NavBar = ({ component, variant,text, type }) => {
         setScrolled(false);
       }
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-
-  const homePage = location.pathname !== '/';
+  const homePage = location.pathname !== "/";
 
   return (
-    <Box sx={{ flexGrow: 1, width: '100%' }}>
+    <Box sx={{ flexGrow: 1, width: "100%" }}>
       <AppBar
-        color={homePage ? 'primary' : scrolled ? 'primary' : 'transparent'}
+        color={homePage ? "primary" : scrolled ? "primary" : "transparent"}
         elevation={scrolled ? 4 : 0}
-        position={variant === 'block' ? 'block' : 'fixed'}
-        sx={{ height: '100px', justifyContent: 'center', alignItems: 'center' }}
+        position={variant === "block" ? "block" : "fixed"}
+        sx={{ height: "100px", justifyContent: "center", alignItems: "center" }}
       >
         <Toolbar
           color="white"
           sx={{
             width: {
-              mobile: '100vw',
-              tablet: '100vw',
-              laptop: '100vw',
+              mobile: "100vw",
+              tablet: "100vw",
+              laptop: "100vw",
               desktop: values.desktop,
             },
           }}
@@ -87,13 +87,13 @@ const NavBar = ({ component, variant,text, type }) => {
             display="flex"
             alignItems="center"
             gap="10px"
-            color={!scrolled ? 'black' : 'white'}
+            color={!scrolled ? "black" : "white"}
             sx={{ flexGrow: 1 }}
           >
             <Link
               href="/"
-              color={!scrolled ? 'inherit' : 'white'}
-              sx={{ display: 'flex', alignItems: 'center' }}
+              color={!scrolled ? "inherit" : "white"}
+              sx={{ display: "flex", alignItems: "center" }}
               underline="none"
             >
               <LocalHospitalIcon color="inherit" />
@@ -101,47 +101,76 @@ const NavBar = ({ component, variant,text, type }) => {
             <Typography
               variant="h5"
               component="div"
-              color={!scrolled ? 'black' : 'white'}
+              color={!scrolled ? "black" : "white"}
               sx={{
-                fontSize: '2rem', // increase the font size to 2rem
+                fontSize: "2rem", // increase the font size to 2rem
               }}
             >
-              <Link href="/" color={!scrolled ? 'inherit' : 'white'} underline="none">
+              <Link
+                href="/"
+                color={!scrolled ? "inherit" : "white"}
+                underline="none"
+              >
                 <Box
                   sx={{
                     textShadow: `${
                       !scrolled
-                        ? '3px 3px 3px rgba(0,0,0,0.5)'
-                        : '2px 2px 2px rgba(255,255,255,0.5)'
+                        ? "3px 3px 3px rgba(0,0,0,0.5)"
+                        : "2px 2px 2px rgba(255,255,255,0.5)"
                     }`,
                   }}
                 >
-                  { text === "Mi Cuenta:"  ? (<> Mi cuenta: {type.nombre}<br/> <span style={{fontSize: '50%'}}>{type.email}</span> </>) : 'CONSUMEDIC' }
-
-                 
+                  {text === "Mi Cuenta:" ? (
+                    <>
+                      {" "}
+                      Mi cuenta: {type.nombre}
+                      <br />{" "}
+                      <span style={{ fontSize: "50%" }}>{type.email}</span>{" "}
+                    </>
+                  ) : (
+                    "CONSUMEDIC"
+                  )}
                 </Box>
               </Link>
             </Typography>
           </Box>
           {screenSizeSmall ? (
-            <DrawerComponent navLinksArray={navLinksArray} scrolled={scrolled}/>
+            <DrawerComponent
+              navLinksArray={navLinksArray}
+              scrolled={scrolled}
+            />
           ) : (
-            <nav style={{ color: `${!scrolled ? 'black' : 'white'}` }}>
-              <Button color="inherit" href={navLinksArray[0].path} sx={{ padding: 2 }}>
+            <nav style={{ color: `${!scrolled ? "black" : "white"}` }}>
+              <Button
+                color="inherit"
+                href={navLinksArray[0].path}
+                sx={{ padding: 2 }}
+              >
                 {navLinksArray[0].title}
               </Button>
 
               {!session.token ? (
                 navLinksArray.slice(1).map((link, index) => (
-                  <Button key={index} color="inherit" href={link.path} sx={{ padding: 2 }}>
+                  <Button
+                    key={index}
+                    color="inherit"
+                    href={link.path}
+                    sx={{ padding: 2 }}
+                  >
                     {link.title}
                   </Button>
                 ))
               ) : (
                 <>
+                  {patientDetail.admin && (
+                    <Button color="inherit" href="/admin">
+                      ADMIN
+                    </Button>
+                  )}
+
                   <Button
                     color="inherit"
-                    href={session.isDoctor ? '/perfilMedico' : '/patientpanel'}
+                    href={session.isDoctor ? "/perfilMedico" : "/patientpanel"}
                   >
                     MI CUENTA
                   </Button>
