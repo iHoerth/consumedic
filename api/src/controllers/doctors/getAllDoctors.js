2e49335b6cee6b9e0e8fc727dc027c3aec1b47d5
iHoerth <hoerth.ignacio@gmail.com>
2023-05-31 02:38:02 -0300

    a ver

--- api/src/controllers/doctors/getAllDoctors.js
+++ api/src/controllers/doctors/getAllDoctors.js
@@ -1,37 +1,42 @@
 const { DoctorType } = require('../../db');
 const { getDoctorCalendar } = require('../appointments/getDoctorCalendar');
-const { getHorariosById } = require('../horarios/getHorariosById');
+
 
 const getAllDoctors = async () => {
   const doctors = await DoctorType.findAll({
     where: { status: 'active' },
     include: { all: true },
   });
-  
-  return doctors;
-  //   doctors.map(async (doctor) => {
-  //     const calendar = await getDoctorCalendar(id);
-  //     doctor.dataValues.calendar = calendar;
 
-  //     const agenda = await getHorariosById(id);
-  //     doctor.dataValues.agenda = agenda;
 
-  //     return doctor;
-  //   });
+    for(let i=0; i<doctors.length;i++){
+      const calendar = await getDoctorCalendar(doctors[i].id)
+      doctors[i].dataValues.calendar = calendar
+    }
 
-  //   return doctors;
-  const doctorPromises = doctors.map(async (doctor) => {
-    const id = doctor.id;
-    const [calendar, agenda] = await Promise.all([getDoctorCalendar(id), getHorariosById(id)]);
+    // doctors.map(async (doctor) => {
+    //   const calendar = await getDoctorCalendar(doctor.id);
+    //   doctor.dataValues.calendar = calendar
+    
+    // })
 
-    doctor.dataValues.calendar = calendar;
-    doctor.dataValues.agenda = agenda;
+    //   return doctor;
+    // });
+    console.log("id",doctors[0].dataValues.id)
+    console.log("calendario", doctors[0].dataValues.calendar);
+    return doctors;
+  // const doctorPromises = doctors.map(async (doctor) => {
+  //   const id = doctor.id;
+  //   const [calendar, agenda] = await Promise.all([getDoctorCalendar(id), getHorariosById(id)]);
 
-    return doctor;
-  });
+  //   doctor.dataValues.calendar = calendar;
+  //   doctor.dataValues.agenda = agenda;
 
-  const resolvedDoctors = await Promise.all(doctorPromises);
-  return resolvedDoctors;
+  //   return doctor;
+  // });
+
+  // const resolvedDoctors = await Promise.all(doctorPromises);
+  // return resolvedDoctors;
 };
 
 module.exports = { getAllDoctors };
