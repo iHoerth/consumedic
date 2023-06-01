c4be5c1fcdf298783823f2380a04dd87e9dd888c
Maurog5 <maurogatica015@gmail.com>
2023-06-01 01:19:21 -0300

    scroling de footer

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,28 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Typography, Link, Box, IconButton, Divider } from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import { NavLink } from 'react-router-dom';
-import { AppBar } from '@mui/material';
 import { useMediaQuery, useTheme } from '@mui/material';
-import { BottomNavigation, BottomNavigationAction, Typography, Box } from '@mui/material';
-import {
-  Home,
-  Info,
-  Lock,
-  Facebook,
-  Comment,
-  Twitter,
-  Instagram,
-  People,
-  RateReview,
-} from '@mui/icons-material';
-import {Tooltip} from '@mui/material'
 import { useLocation } from 'react-router-dom';
+
 const preventDefault = (event) => event.preventDefault();
 
 const Footer = () => {
-  const [currentPage, setCurrentPage] = useState('home');
   const theme = useTheme();
+  const [currentPage, setCurrentPage] = useState('home');
   const location = useLocation();
   const doctorDashboard = location.pathname !== '/perfilMedico';
+
+  useEffect(() => {
+    setCurrentPage(location.pathname === '/' ? 'home' : '');
+  }, [location]);
 
   const handleNavigation = (event, page) => {
     let newHeight = 0;
@@ -33,192 +28,132 @@ const Footer = () => {
       case 'about':
         newHeight = theme.heights.homeSection * 1;
         break;
-      case 'testimonials':
-        newHeight = theme.heights.homeSection * 2;
-        break;
       case 'team':
-        newHeight = theme.heights.homeSection * 2;
+        newHeight = theme.heights.homeSection * 1.5;
         break;
     }
     const scrollTo = newHeight; // Valor de desplazamiento personalizado (en este caso, 40% de la altura de la ventana)
     window.scrollTo({ top: scrollTo, behavior: 'smooth' });
     setCurrentPage(page);
-
   };
 
   return (
-    <AppBar
-      sx={{
-        position: 'relative',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-      color={location.pathname === '/perfilMedico' ? 'secondary' : 'primary'}
+    <footer
+    style={{
+      backgroundColor: theme.palette.primary.main,
+      paddingTop: '35px',
+      marginTop: 'auto',
+      textAlign: 'center',
+      padding: '10px',
+      scrollBehavior: 'smooth', // Hace que el scrolling sea más suave y lento
+    }}
     >
-      <Box
-        sx={{
-          p: 1,
-          width: '100%',
-          // bgcolor: theme.palette.primary.main,
-        }}
-        color={location.pathname === '/perfilMedico' ? 'secondary' : 'primary'}
-
-      >
-        <BottomNavigation
+      <Box display="flex" justifyContent="center" mt={2}>
+        <Link
           value={currentPage}
           onChange={handleNavigation}
+         
+          color="inherit"
+          variant="body2"
           sx={{
-            bgcolor: location.pathname === '/perfilMedico' ? theme.palette.secondary.main : theme.palette.primary.main
+            pl: '40px',
+            pr: '40px',
+            fontWeight: 700,
+            textDecoration: 'none',
+            color: 'white',
           }}
-
+          onClick={handleNavigation}
         >
-          <Tooltip title="Inicio" placement="top">
-            <BottomNavigationAction
-              color="white"
-              showLabel="true"
-              icon={
-                <Home
-                  sx={{
-                    width: 24,
-                    height: 24,
-                    color: 'white',
-                  }}
-                />
-              }
-              href={window.location.pathname !== '/' ? '/' : ''}
-            />
-          </Tooltip>
-          <Tooltip title="Sobre Nosotros" placement="top">
-          <BottomNavigationAction
-            icon={
-              <Info
-                sx={{
-                  width: 22,
-                  height: 22,
-                  color: 'white',
-                }}
-              />
-            }
-            value="about"
-          />
-          </Tooltip>
-          <Tooltip title="Testimonios" placement="top">
-          <BottomNavigationAction
-            icon={
-              <Comment
-                sx={{
-                  width: 22,
-                  height: 22,
-                  color: 'white',
-                }}
-              />
-            }
-            value="testimonials"
-          />
-          </Tooltip>
-          <Tooltip title="Equipo" placement="top">
-          <BottomNavigationAction
-            label="Equipo"
-            icon={
-              <People
-                sx={{
-                  width: 22,
-                  height: 22,
-                  color: 'white',
-                }}
-              />
-            }
-            value="team"
-          />
-          </Tooltip>
-          
-          <Tooltip title="Login" placement="top">
-          <a href="/login">
-            <BottomNavigationAction
-              label="Login"
-              icon={
-                <Lock
-                  sx={{
-                    width: 22,
-                    height: 22,
-                    color: 'white',
-                  }}
-                />
-              }
-              value="login"
-            />
-          </a></Tooltip>
-        </BottomNavigation>
-        
+          INICIO
+        </Link>
+        <Link
+          value="about"
+          color="inherit"
+          variant="body2"
+          sx={{
+            pl: '40px',
+            pr: '40px',
+            fontWeight: 700,
+            textDecoration: 'none',
+            color: 'white',
+          }}
+          onClick={(event) => handleNavigation(event, 'about')}
+        >
+          SOBRE NOSOTROS
+        </Link>
+        <Link
+          href="/login"
+          color="inherit"
+          variant="body2"
+          sx={{
+            pl: '40px',
+            pr: '40px',
+            fontWeight: 700,
+            textDecoration: 'none',
+            color: 'white',
+          }}
+          onChange={handleNavigation}
+        >
+          LOGIN
+        </Link>
+        <Link
+          value="team"
+          variant="body2"
+          sx={{
+            pl: '40px',
+            pr: '40px',
+            fontWeight: 700,
+            textDecoration: 'none',
+            color: 'white',
+          }}
+          onClick={(event) => handleNavigation(event, 'team')}
+        >
+          EQUIPO
+        </Link>
       </Box>
-      <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          // bgcolor: theme.palette.primary.main,
-        }}
-        color={location.pathname === '/perfilMedico' ? 'secondary' : 'primary'}
-      >
-        <Tooltip title="Facebook" placement="top">
-        <NavLink to="https://www.facebook.com/profile.php?id=100092882291994" target="_blank">
-          <Facebook
-            sx={{
-              mx: 1,
-              color: 'white',
-              fontSize: '30px',
-              ml: '20px',
-              mr: '30px',
-            }}
-          />
-        </NavLink>
-        </Tooltip>
-        <Tooltip title="Twitter" placement="top">
-        <NavLink to="https://www.twitter.com/" target="_blank">
-          <Twitter
-            sx={{
-              mx: 1,
-              color: 'white',
-              fontSize: '30px',
-              ml: '30px',
-              mr: '30px',
-            }}
-          />
-        </NavLink>
-        </Tooltip>
-        <Tooltip title="Instagram" placement="top">
-        <NavLink to="https://www.instagram.com/pf.consumedic/" target="_blank">
-          <Instagram
-            sx={{
-              mx: 1,
-              color: 'white',
-              fontSize: '30px',
-              ml: '30px',
-              mr: '30px',
-            }}
-          />
-        </NavLink>
-        </Tooltip>
+      <Box display="flex" width="100%" justifyContent="center" mt={2}>
+      <Divider style={{ width: '25%', marginTop: '10px' }} />
       </Box>
-      <Box
-        sx={{
-          p: 2,
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography variant="body2" align="center" color="white">
-          {`Copyright © ${new Date().getFullYear()}
-          Consumedic. Encontrá tu especialista y pedí turno`}
+
+      <Box display="flex" width="100%" justifyContent="center" mt={2}>
+        <Typography
+          variant="body2"
+          align="center"
+          style={{ width: '25%', color: 'white' }}
+        >
+          Nuestro objetivo es brindar un servicio eficiente y conveniente para agendar turnos médicos en línea. En nuestra plataforma, puedes encontrar y reservar citas con profesionales de la salud en diversos campos, ofreciéndote comodidad y flexibilidad en la gestión de tu atención médica.
         </Typography>
       </Box>
-    </AppBar>
+
+      <Box display="flex" width="100%" justifyContent="center" mt={2}>
+        <Divider style={{ width: '25%', marginTop: '10px' }} />
+      </Box>
+
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        marginTop="10px"
+      >
+        <NavLink to="https://www.facebook.com/profile.php?id=100092882291994" target="_blank">
+          <FacebookIcon sx={{ color: 'white', margin: '10px' }} />
+        </NavLink>
+
+        <NavLink to="https://www.twitter.com/" target="_blank">
+          <TwitterIcon sx={{ color: 'white', margin: '10px' }} />
+        </NavLink>
+
+        <NavLink to="https://www.instagram.com/pf.consumedic/" target="_blank">
+          <InstagramIcon sx={{ color: 'white', margin: '10px' }} />
+        </NavLink>
+      </Box>
+
+      <Box display="flex" justifyContent="center" alignItems="center">
+        <Typography variant="body2" color="White" align="center" padding="10px">
+          &copy; {new Date().getFullYear()} Consumedic. Encontrá tu especialista y pedí turno
+        </Typography>
+      </Box>
+    </footer>
   );
 };
 
